59918bf86d58a70bb99edd321b67511e52ccf5db
matiboy <mathieu.chauvinc@gmail.com>
2015-07-23 15:38:16 +0800

    Fix metadata

--- setup.py
+++ setup.py
@@ -5,8 +5,7 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the relevant file
-with open(path.join(here, 'DESCRIPTION.md'), encoding='utf-8') as f:
-    long_description = f.read()
+long_description = 'See README.md'
 
 setup(
     name='django-admin-methods',
